b9a1080a00acf39b13e57a5e5d905e58a8fcbb1f
Maximus Aurelius Wiranata <maximusaureliusw@gmail.com>
2022-05-02 13:02:32 +0700

    background

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title')</title>
+    <link rel="icon" href="img/logocareerexpo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-image: url(img/bg.svg); background-size: cover">
+    @include('layout.header')
+    <div class="" style="min-height: 80vh">
+        @yield('content')
+    </div>
+    @include('layout.footer')
 
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
