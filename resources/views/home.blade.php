08adcf15ffff36935df9d645aff19eb91978cc30
Maximus Aurelius Wiranata <maximusaureliusw@gmail.com>
2022-05-30 16:39:00 +0700

    update testi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,7 +59,7 @@
 
 <h2 class="text-center mb-4 mt-5">Testimonial</h2>
 <div class="justify-content-center flexhidden">  
-  <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel" style="width: 90%">
+  {{-- <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel" style="width: 90%">
     <div class="d-flex justify-content-center">
       <div class="carousel-inner" style="width: 80%">            
         <div class="carousel-item active">        
@@ -190,7 +190,76 @@
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
-  </div>
+  </div> --}}
+  <div class="row widthpersen2">
+    <div class="col">
+      <div class="" style="width: 100%;">
+            <img src="img/testi/anetta.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h2 class="card-title text-center">Anetta TW</h2>
+              <p class="card-text text-center">Menurutku acaranya
+                insightful bgt. Perusahaannya juga
+                variatif jadi aku banyak dapet
+                insight juga tentang perusahaan2
+                yang mungkin sebelumnya aku
+                belum pernah denger. Selain itu
+                acaranya juga interaktif, kita
+                dikasih kesempatan buat tanya2 ke
+                narasumber langsung. Kita juga
+                dibantu sama website virtual
+                career expo buat apply2 ke
+                perusahaan yang kita pengen.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+      <div class="" style="width: 100%;">
+            <img src="img/testi/berliana.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h2 class="card-title text-center">Berliana Fira</h2>
+              <p class="card-text text-center">Menurutku career expo virtual asik
+                dan gak merepotkan untuk antri dll
+                untuk cari kerja dan mendukung
+                kemajuan teknologi. Tapi
+                menurutku kekurangan tahun kmrn
+                adalah keterbatasan untuk
+                mendapatkan info lowongan pada
+                setiap perusahaan karena
+                keterbatasan saat dibagi breakout
+                room itu jadi gak bisa join banyak
+                hehe dan kalo gak salah kmrn
+                untuk ngelamar juga dibatasi.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+      <div class="" style="width: 100%;">
+            <img src="img/testi/sukma.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h2 class="card-title text-center">Sukma Jenny</h2>
+              <p class="card-text text-center">UC Virtual Career Expo
+                memperlihatkan saya pada
+                informasi-informasi terkait karir
+                dan hal-hal yang meliputinya.
+                Berbagai korporasi yang terlibat
+                cukup representatif lintas bidang
+                dan orientasi. Tidak sebatas
+                informasi, tetapi tautan yang
+                disediakan Uc Career Expo dirasa
+                dapat mengkatalisasi pergerakan
+                dari rekan-rekan yang sedang
+                mempersiapkan karirnya. Apresiasi
+                tinggi bagi panitia yang telah
+                menyusun acara dengan begitu
+                apik, terstruktur, dan tidak
+                prematur.
+                </p>
+            </div>
+        </div>
+    </div>
+  </div>  
 </div>
 
 <div class="justify-content-center hiddenflex">  
@@ -199,28 +268,68 @@
       <div class="carousel-inner" style="width: 80%">            
         <div class="carousel-item active">        
           <div class="" style="width: 100%;">
-            <img src="img/dummy.svg" class="card-img-top" alt="...">
+            <img src="img/testi/anetta.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-              <h2 class="card-title text-center">Nama</h2>
-              <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h2 class="card-title text-center">Anetta TW</h2>
+              <p class="card-text text-center">Menurutku acaranya
+                insightful bgt. Perusahaannya juga
+                variatif jadi aku banyak dapet
+                insight juga tentang perusahaan2
+                yang mungkin sebelumnya aku
+                belum pernah denger. Selain itu
+                acaranya juga interaktif, kita
+                dikasih kesempatan buat tanya2 ke
+                narasumber langsung. Kita juga
+                dibantu sama website virtual
+                career expo buat apply2 ke
+                perusahaan yang kita pengen.
+                </p>
             </div>
           </div>            
         </div>
         <div class="carousel-item">
           <div class="" style="width: 100%;">
-            <img src="img/dummy.svg" class="card-img-top" alt="...">
+            <img src="img/testi/berliana.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-              <h2 class="card-title text-center">Nama</h2>
-              <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h2 class="card-title text-center">Berliana Fira</h2>
+              <p class="card-text text-center">Menurutku career expo virtual asik
+                dan gak merepotkan untuk antri dll
+                untuk cari kerja dan mendukung
+                kemajuan teknologi. Tapi
+                menurutku kekurangan tahun kmrn
+                adalah keterbatasan untuk
+                mendapatkan info lowongan pada
+                setiap perusahaan karena
+                keterbatasan saat dibagi breakout
+                room itu jadi gak bisa join banyak
+                hehe dan kalo gak salah kmrn
+                untuk ngelamar juga dibatasi.
+                </p>
             </div>
           </div>  
         </div>
         <div class="carousel-item">
           <div class="" style="width: 100%;">
-            <img src="img/dummy.svg" class="card-img-top" alt="...">
+            <img src="img/testi/sukma.jpeg" class="card-img-top" alt="...">
             <div class="card-body">
-              <h2 class="card-title text-center">Nama</h2>
-              <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h2 class="card-title text-center">Sukma Jenny</h2>
+              <p class="card-text text-center">UC Virtual Career Expo
+                memperlihatkan saya pada
+                informasi-informasi terkait karir
+                dan hal-hal yang meliputinya.
+                Berbagai korporasi yang terlibat
+                cukup representatif lintas bidang
+                dan orientasi. Tidak sebatas
+                informasi, tetapi tautan yang
+                disediakan Uc Career Expo dirasa
+                dapat mengkatalisasi pergerakan
+                dari rekan-rekan yang sedang
+                mempersiapkan karirnya. Apresiasi
+                tinggi bagi panitia yang telah
+                menyusun acara dengan begitu
+                apik, terstruktur, dan tidak
+                prematur.
+                </p>
             </div>
           </div>   
         </div>
